f839b67b77b20062e8720fdd9e7bdd4fef43fa47
Bernardo Campos <bcampos@iqs.cl>
2018-02-13 10:55:35 -0300

    loginMeetings

--- src/_services/user.service.js
+++ src/_services/user.service.js
@@ -1,55 +1,74 @@
-import { authHeader } from '../_helpers';
+import { authHeader } from "../_helpers";
 
 export const userService = {
-    login,
-    logout,
-    getAll
+  login,
+  logout,
+  getAll
 };
 
 function login(username, password) {
-    const requestOptions = {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ username, password })
-    };
+  var params = {
+    client_id: "UserClient",
+    client_secret: "usersecret",
+    grant_type: "password",
+    username: username,
+    password: password
+  };
 
-    return fetch('/users/authenticate', requestOptions)
-        .then(response => {
-            if (!response.ok) { 
-                return Promise.reject(response.statusText);
-            }
+  var formData = new FormData();
 
-            return response.json();
-        })
-        .then(user => {
-            // login successful if there's a jwt token in the response
-            if (user && user.token) {
-                // store user details and jwt token in local storage to keep user logged in between page refreshes
-                localStorage.setItem('user', JSON.stringify(user));
-            }
+  for (var k in params) {
+    formData.append(k, params[k]);
+  }
 
-            return user;
-        });
+  return axios
+    .post(
+      "https://identityindustries.azurewebsites.net/Connect/Token",
+      formData
+    )
+    .then(function(response) {
+      console.log(response.data);
+      if (response.data) {
+        const AuthStr = "Bearer ".concat(response.data.access_token);
+        sessionStorage.setItem("token", JSON.stringify(response.data));
+        axios
+          .get(
+            "https://identityindustries.azurewebsites.net/Connect/UserInfo",
+            { headers: { Authorization: AuthStr } }
+          )
+          .then(function(responseInfo) {
+            sessionStorage.setItem(
+              "user",
+              JSON.stringify(responseInfo.data.sub)
+            );
+          });
+      }
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
+
+  return user;
 }
 
 function logout() {
-    // remove user from local storage to log user out
-    localStorage.removeItem('user');
+  // remove user from local storage to log user out
+  sessionStorage.removeItem("user");
 }
 
 function getAll() {
-    const requestOptions = {
-        method: 'GET',
-        headers: authHeader()
-    };
+  const requestOptions = {
+    method: "GET",
+    headers: authHeader()
+  };
 
-    return fetch('/users', requestOptions).then(handleResponse);
+  return fetch("/users", requestOptions).then(handleResponse);
 }
 
 function handleResponse(response) {
-    if (!response.ok) { 
-        return Promise.reject(response.statusText);
-    }
+  if (!response.ok) {
+    return Promise.reject(response.statusText);
+  }
 
-    return response.json();
+  return response.json();
 }
